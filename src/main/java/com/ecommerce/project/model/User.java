7ee7b0b05454ac0c6b5482ae3b3cc7ff33f06274
fahimshaik36 <fahimshaik636@gmail.com>
2026-08-26 11:01:43 +0530

    Implement entity relationships for users

--- src/main/java/com/ecommerce/project/model/User.java
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -7,6 +7,9 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -34,4 +37,11 @@ public class User {
         this.email = email;
         this.password = password;
     }
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+                fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 }
