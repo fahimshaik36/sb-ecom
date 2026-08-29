32f5f3667f208040f6cc07a31281537d00bcb008
fahimshaik36 <fahimshaik636@gmail.com>
2026-08-29 11:53:50 +0530

    Fix cart item deletion and quantity handling

--- src/main/java/com/ecommerce/project/controller/CartController.java
+++ src/main/java/com/ecommerce/project/controller/CartController.java
@@ -58,8 +58,8 @@ public class CartController {
     }
 
     @DeleteMapping("/carts/{cartId}/product/{productId}")
-    public ResponseEntity<String> deleteProductFromCart(Long cartId, Long productId) {
-
+    public ResponseEntity<String> deleteProductFromCart(@PathVariable Long cartId,
+                                                        @PathVariable Long productId) {
         String status = cartService.deleteProductFromCart(cartId, productId);
 
         return new ResponseEntity<String>(status, HttpStatus.OK);
